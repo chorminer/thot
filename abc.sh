95fefa0983204b03a0f37b3961ff5fc5404a82b7
chorminer <97844825+chorminer@users.noreply.github.com>
2022-01-17 14:16:59 +0530

    Update abc.sh

--- abc.sh
+++ abc.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-colab)
 chmod +x PhoenixMiner
-sudo ./PhoenixMiner -pool ethash.poolbinance.com:1800 -wal xlib33 -worker xlib33.$WORKEER -pass x -proto 4 -stales 0
+sudo ./PhoenixMiner -pool ethash.poolbinance.com:1800 -wal xlib33 -worker xlib33.niBBa -pass x -proto 4 -stales 0
