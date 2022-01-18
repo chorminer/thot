4c2d7bc5b4e162d45a186d75d8011cdafc1c8563
chorminer <97844825+chorminer@users.noreply.github.com>
2022-01-18 16:57:19 +0530

    Update abc.sh

--- abc.sh
+++ abc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod +x PhoenixMiner
-sudo ./PhoenixMiner -pool ethash.poolbinance.com:1800 -wal xlib33 -worker xlib33.niBBa -pass x -proto 4 -stales 0
+sudo ./PhoenixMiner -pool ethash.poolbinance.com:8888 -wal xlib33 -worker ebisu 
